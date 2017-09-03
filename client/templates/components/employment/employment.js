87ce723f2df7d425d16729d839a0e5c1ce9982a6
simeon <simeon@digitalescrowservices.com>
2017-09-03 04:34:10 -0400

    CONTENT!!!!!

--- client/templates/components/employment/employment.js
+++ client/templates/components/employment/employment.js
@@ -23,32 +23,38 @@ if (Meteor.isClient) {
     },
     jobs: function () {
       var simeon_jobs = [
-        { "title": "Job Title", "company": "Bitheads Inc.", "img": "bithead.png",
+        { "title": "CTO, Lead Developer", "company": "Digital Escrow Services", "img": "bithead.png",
           "content": {
-            "dates": "month 2000 - month 2000",
-            "points": [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                        "Praesent vestibulum dolor sollicitudin dolor sollicitudin, vel ullamcorper metus gravida. Quisque egestas risus ipsum, ac placerat lacus efficitur eget.",
-                        "Sed volutpat ex eget volutpat pharetra. Vivamus in bibendum urna, vel suscipit metus."
-                      ]
+            "dates": "Sept 2015 - Aug 2017",
+            "points": [
+              "Responsible for implementation, and management of all code and infrastructure for a small tech startup.",
+              "Developed a mobile application from scratch using a JS stack and Meteor, also produced and maintained several websites.",
+              "Responsible for the entire lifecycle of mobile and web-app development from conception and design to deployment and maintainance.",
+            ]
           }
         },
-        { "title": "Job Title", "company": "Bitheads Inc.", "img": "bithead.png",
+        { "title": "Developer", "company": "CNW-Mediavantage", "img": "bithead.png",
           "content": {
-            "dates": "month 2000 - month 2000",
-            "points": [ "Fusce egestas orci elit, sed ultricies nunc sollicitudin vel. Nunc et eleifend lacus, at molestie augue.",
-                        "Vestibulum sed pellentesque felis. Curabitur eu est scelerisque, laoreet neque quis, vestibulum velit.",
-                        "Vestibulum sed pellentesque felis. Curabitur eu est scelerisque, laoreet neque quis, vestibulum velit.",
-                        "Nullam felis metus, fermentum eu ex eget, ornare lacinia metus. Fusce ullamcorper erat et turpis finibus, nec rhoncus quam viverra."
-                      ]
+            "dates": "Sept 2011 - Dec 2015",
+            "points": [
+              "Responsible for code architecture and development and deployment.",
+              "Actively participated in an agile team eventually progressing to limited leadership roles.",
+              "Progressed from Co-op student all the way to full team member over 8 months.",
+              "Responsible for maintaining the entire version control, CBI, and deployment systems.",
+              "Heavily involved in several enterprise-level development projects including a migration to Elasticsearch/Hadoop/Samza, several OS upgrades, and integrating the Twitter feed to our product.",
+              "Part of a team of two responsible for new uOttawa co-op students. This included the hiring process, onboarding and training, professional development, review, and assisting with final reports.",
+            ]
           }
         },
-        { "title": "Job Title", "company": "Bitheads Inc.", "img": "bithead.png",
+        { "title": "Game developer / security expert", "company": "Carleton University", "img": "bithead.png",
           "content": {
-            "dates": "month 2000 - month 2000",
-            "points": [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                        "Vestibulum sed pellentesque felis. Curabitur eu est scelerisque, laoreet neque quis, vestibulum velit.",
-                        "Donec mauris massa, ullamcorper nec eros sit amet, tristique viverra neque."
-                      ]
+            "dates": "Jan - Jul 2011",
+            "points": [
+              "6-month co-op term for the Carleton Human Computer Interaction lab developing a game to teach youth about fundamental security practices.",
+              "Developed in PHP, python, and Flash-Actionscript.",
+              "Assisted professors and graduate students in the gamification of their research findings.",
+              "Responsible for graphic and game design, code architecture, and necessary infrastructure.",
+            ]
           }
         },
       ];
--- client/templates/components/skills/skills.js
+++ client/templates/components/skills/skills.js
@@ -16,15 +16,51 @@ if (Meteor.isClient) {
     skills: function () {
       var simeon_skills = [
         {"title": "Development", "icon": "code",
-          "skills": [
-            {"text": "Lorem ipsum", "stars": 5},
-            {"text": "Lorem ipsum", "stars": 5},
-            {"text": "Lorem ipsum", "stars": 5},
-            {"text": "Lorem ipsum", "stars": 5},
+          "skill": [
+            {"text": "Learning new languages", "stars": 5},
+            {"text": "Java/JS", "stars": 5},
+            {"text": "HTML5+CSS3", "stars": 5},
+            {"text": "Meteor (multi-platform mobile apps)", "stars": 5},
+            {"text": "MongoDB", "stars": 5},
+            {"text": "Coding for the future", "stars": 4},
+            {"text": "REST APIs", "stars": 4},
+            {"text": "SQL + othe DBs", "stars": 4},
+            {"text": "JUnit/JMock/Mockito/Jasmine", "stars": 3},
+            {"text": "Highcharts/Plotly.js", "stars": 3},
+            {"text": "Nagios", "stars": 3},
+            {"text": "Scripting (bash, python)", "stars": 2},
+            {"text": "Samza/Kafka straming", "stars": 1},
+            {"text": "Elastic Search", "stars": 1},
+            {"text": "Hadoop", "stars": 1},
           ]
         },
-        {"title": "Technology", "icon": "computer"},
-        {"title": "Professional", "icon": "group"},
+        {"title": "Technology", "icon": "computer",
+          "skill": [
+            {"text": "Linux", "stars": 5},
+            {"text": "git+svn services", "stars": 5},
+            {"text": "Ethereum+Solidity", "stars": 5},
+            {"text": "Blockchain", "stars": 4},
+            {"text": "Jenkins/Bamboo CBI", "stars": 4},
+            {"text": "Rally/Bugzilla", "stars": 4},
+            {"text": "GIMP/Photoshop/Illustator", "stars": 4},
+            {"text": "Zendesk/Trello/Jira", "stars": 3},
+            {"text": "Windows", "stars": 2},
+            {"text": "'Big Data'", "stars": 2},
+          ]
+        },
+        {"title": "Professional", "icon": "group",
+          "skill": [
+            {"text": "Lean/Agile/SCRUM teamwork", "stars": 5},
+            {"text": "Independent learning", "stars": 5},
+            {"text": "Work ethic", "stars": 5},
+            {"text": "Leadership", "stars": 4},
+            {"text": "Whiteboarding", "stars": 4},
+            {"text": "Teaching", "stars": 4},
+            {"text": "Story writing and documentation", "stars": 4},
+            {"text": "Test Driven Development", "stars": 3},
+            {"text": "Client support", "stars": 3},
+          ]
+        },
       ];
 
       var logan_skills = [
